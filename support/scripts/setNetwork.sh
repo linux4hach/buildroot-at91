2c4862fa10f2404e91bd78711fd7409a10325469
cfair <cfair@hach.com>
2014-06-03 13:50:47 -0600

    Finished work on setting up after run scripts

--- support/scripts/setNetwork.sh
+++ support/scripts/setNetwork.sh
@@ -1,11 +1,12 @@
 #!/bin/bash 
 INTERFACES_FILE="${TARGET_DIR}/etc/network/interfaces"
 SEARCH_CRITERIA="eth0"
-TRUE_VALUE="0"
+NONE_FOUND="0"
 
 RESULT_FROM_COMPARISON=$(grep -c $SEARCH_CRITERIA $INTERFACES_FILE)
+echo "$RESULT_FROM_COMPARISON"
 
-if [ "$RESULT_FROM_COMPARISON" == "$TRUE_VALUE" ] ;
+if [ "$RESULT_FROM_COMPARISON" == "$NONE_FOUND" ] 
 then
    echo "auto eth0" >> $INTERFACES_FILE 
    echo "iface eth0 inet dhcp" >> $INTERFACES_FILE
