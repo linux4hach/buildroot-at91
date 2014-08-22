bc7762f8338708c295c93badc890e401fbe99a85
cfair <cfair@hach.com>
2014-08-22 09:41:39 -0600

    Changed the server name 	modified:   updateRFS.sh

--- updateRFS.sh
+++ updateRFS.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-echo "Deleting old rootfs in /mnt/tftpboot/rootfs"
-rm -rf /mnt/tftpboot/rootfs/*
+echo "Deleting old rootfs in /server/tftpboot/rootfs"
+rm -rf /server/tftpboot/rootfs/*
 
-echo "Uncompressing rootfs to /mnt/tftpboot/rootfs" 
-tar zxvf output/images/rootfs.tar.gz -C /mnt/tftpboot/rootfs 
+echo "Uncompressing rootfs to /server/tftpboot/rootfs" 
+tar zxvf output/images/rootfs.tar.gz -C /server/tftpboot/rootfs 
 
 echo "Changing permissions to nobody"
-chown  -R nobody /mnt/tftpboot
+chown  -R nobody /server/tftpboot
 
 echo "Changing the var/empty directory to have no permissions for group or all write"
-chmod -R ag-w /mnt/tftpboot/rootfs/var/empty
-chown -R root /mnt/tftpboot/rootfs/var/empty
+chmod -R ag-w /server/tftpboot/rootfs/var/empty
+chown -R root /server/tftpboot/rootfs/var/empty
 
