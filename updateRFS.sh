23325696e661432810b0ef321018452c7f6faea8
cfair <cfair@hach.com>
2014-10-16 09:16:20 -0600

    Added strace support and fixed updateRFS for what most are using

--- updateRFS.sh
+++ updateRFS.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
 echo "Deleting old rootfs in /server/tftpboot/rootfs"
-rm -rf /server/tftpboot/rootfs/*
+rm -rf /srv/tftpboot/rootfs/*
 
 echo "Uncompressing rootfs to /server/tftpboot/rootfs" 
 tar zxvf output/images/rootfs.tar.gz -C /server/tftpboot/rootfs 
 
 echo "Changing permissions to nobody"
-chown  -R nobody /server/tftpboot
+chown  -R nobody /srv/tftpboot
 
 echo "Changing the var/empty directory to have no permissions for group or all write"
-chmod -R ag-w /server/tftpboot/rootfs/var/empty
-chown -R root /server/tftpboot/rootfs/var/empty
+chmod -R ag-w /srv/tftpboot/rootfs/var/empty
+chown -R root /srv/tftpboot/rootfs/var/empty
 
