ed6e9d230eecd673ff2d4e5f49839962aaf02876
cfair <cfair@hach.com>
2014-12-29 09:42:53 -0700

    I altered the make file for liboping.mk to point to http://chemicalinventory.hach.ewqg.com/files instead of the redirected site.   This allows us to retrieve the library without pestering IT 	modified:   package/liboping/liboping.mk

--- package/liboping/liboping.mk
+++ package/liboping/liboping.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LIBOPING_VERSION = 1.6.2
-LIBOPING_SITE = http://verplant.org/liboping/files
+LIBOPING_SITE = http://chemicalinventory.hach.ewqg.com/files
 LIBOPING_INSTALL_STAGING = YES
 LIBOPING_DEPENDENCIES = $(if $(BR2_PACKAGE_NCURSES),ncurses)
 LIBOPING_CONF_OPT = --without-perl-bindings
