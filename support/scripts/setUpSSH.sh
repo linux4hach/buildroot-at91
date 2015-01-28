a76246f759273e826683adbad1ed930c0b7c1eca
cfair <cfair@hach.com>
2015-01-28 10:28:46 -0700

    modified:   .config 	new file:   support/scripts/setUpSSH.sh

---
+++ support/scripts/setUpSSH.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+SEARCH_CRITERIA="Ciphers aes128-cbc"
+PROFILE="${TARGET_DIR}/etc/ssh/sshd_config"
+RESULTS_OF_SEARCH=$(grep -c $SEARCH_CRITERIA $PROFILE)
+NONE_FOUND="0"
+
+echo "$SEARCH_CRITERIA"
+echo "$PROFILE"
+echo "$RESULTS_OF_SEARCH"
+
+
+
+if [ "$RESULTS_OF_SEARCH" == "$NONE_FOUND" ]
+then 
+   echo -e "\n" >> $PROFILE
+   echo "# I added this Cipher in order to support ssh on our target" >> $PROFILE
+   echo "Ciphers aes128-cbc" >> $PROFILE
+
+fi
+
