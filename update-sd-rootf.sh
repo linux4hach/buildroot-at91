34f4727fa105c3541af894f3f0b3d963dc94f241
ryang <ryang@hach.com>
2015-03-05 12:56:05 -0700

    Updated the buildroot def configuration file to create the squash and jffs2 root files.

--- update-sd-rootf.sh
+++ update-sd-rootf.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-echo "Deleting old rootfs in /media/ryang/rootfs"
-rm -rf /media/ryang/rootfs/*
+echo "Deleting old rootfs in /media/user/rootfs"
+rm -rf /media/$1/rootfs/*
 
-echo "Uncompressing rootfs to /media/ryang/rootfs" 
-tar zxvf output/images/rootfs.tar.gz -C /media/ryang/rootfs 
+echo "Uncompressing rootfs to /media/user/rootfs" 
+tar zxvf output/images/rootfs.tar.gz -C /media/$1/rootfs 
 
 
 echo "Changing the var/empty directory to have no permissions for group or all write"
-chmod -R ag-w /media/ryang/rootfs/var/empty
-chown -R root /media/ryang/rootfs/var/empty
+chmod -R ag-w /media/$1/rootfs/var/empty
+chown -R root /media/$1/rootfs/var/empty
 
