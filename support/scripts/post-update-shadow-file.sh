80105fab16d57f4c346b549be881b2d9fa220bf5
dfroien <dfroien@hach.com>
2016-04-13 11:04:27 -0600

    updated to address security modified:   support/scripts/post-update-shadow-file.sh modified:   system/system.mk

--- support/scripts/post-update-shadow-file.sh
+++ support/scripts/post-update-shadow-file.sh
@@ -18,30 +18,33 @@
 #                    within the 'system' section of the configure system
 
 BUILDROOT_DIR=${PWD}
-BUILDROOT_CONFIGS_DIR=$BUILDROOT_DIR/configs
-BUILDROOT_OUTPUT_DIR=$BUILDROOT_DIR/output
-BUILDROOT_OUTPUT_TARGET_DIR=$BUILDROOT_OUTPUT_DIR/target
-TARGET_CONFIG_NAME=".config"
-TARGET_CONFIG_FILE=$BUILDROOT_DIR/${TARGET_CONFIG_NAME}
+TARGET_CONFIG_NAME="${BUILDROOT_CONFIG}"
 
-if [ -d $BUILDROOT_DIR -a -f $TARGET_CONFIG_FILE ]
+if [ -d $BUILDROOT_DIR -a -f "${BUILDROOT_DIR}/.config" ]
 then
-    source $TARGET_CONFIG_FILE
+    source "${BUILDROOT_DIR}/.config"
 else
     exit 23 # could not find the config file
 fi
 
+BUILDROOT_CONFIGS_DIR=${CONFIG_DIR}
+BUILDROOT_OUTPUT_DIR=$BUILDROOT_DIR/output
+BUILDROOT_OUTPUT_TARGET_DIR=$BUILDROOT_OUTPUT_DIR/target
+
 ETC_DIR=etc
-ORIGINAL_ETC_DIR="${BR2_ROOTFS_OVERLAY}/${ETC_DIR}"
-HACH_ETC_DIR=usr/share/hach/etc
-TARGET_ETC_DIR="$BUILDROOT_OUTPUT_TARGET_DIR/$ETC_DIR"
-RFS_HACH_ETC_DIR="${BR2_ROOTFS_OVERLAY}/$HACH_ETC_DIR"
-SHADOW=shadow
-RFS_ETC_SHADOW_FILE="${RFS_HACH_ETC_DIR}/${SHADOW}"
-TARGET_SHADOW_FILE="$TARGET_ETC_DIR/$SHADOW"
-HACH_SHADOW_FILE="$RFS_HACH_ETC_DIR/$SHADOW"
 PASSWD=passwd
-RFS_PASSWD_FILE="${BR2_ROOTFS_OVERLAY}/$ETC_DIR/$PASSWD"
+SHADOW=shadow
+
+RFS_ETC_DIR="${BR2_ROOTFS_OVERLAY}/${ETC_DIR}"
+RFS_ETC_SHADOW_FILE="$RFS_ETC_DIR/$SHADOW"
+RFS_ETC_PASSWD_FILE="${BR2_ROOTFS_OVERLAY}/$ETC_DIR/$PASSWD"
+
+HACH_ETC_DIR=usr/share/hach/etc
+RFS_HACH_ETC_DIR="${BR2_ROOTFS_OVERLAY}/$HACH_ETC_DIR"
+RFS_HACH_ETC_SHADOW_FILE="${RFS_HACH_ETC_DIR}/${SHADOW}"
+
+BR_TARGET_ETC_DIR="$BUILDROOT_OUTPUT_TARGET_DIR/$ETC_DIR"
+BR_TARGET_SHADOW_FILE="${BR_TARGET_ETC_DIR}/$SHADOW"
 
 CAT=$(which cat)
 CP=$(which cp)
@@ -54,19 +57,19 @@ MV=$(which mv)
 RM=$(which rm)
 
 
-#! copy the shadow file from the target location
+#! copy the shadow file from the buildroot target location
 #! to a read/write location on the root filesystem
 pusf__copy_shadow_file()
 {
     if [ -n $CP ]
     then
-        if [ -n ${BR2_ROOTFS_OVERLAY} -a -f $TARGET_SHADOW_FILE ]
+        if [ -n ${BR2_ROOTFS_OVERLAY} -a -f $BR_TARGET_SHADOW_FILE ]
         then
             if [ -n $DIRNAME ]
             then
-                if [ -d $($DIRNAME ${HACH_SHADOW_FILE}) ]
+                if [ -d $($DIRNAME ${RFS_HACH_ETC_SHADOW_FILE}) ]
                 then
-                    $CP $TARGET_SHADOW_FILE ${HACH_SHADOW_FILE}
+                    $CP $BR_TARGET_SHADOW_FILE ${RFS_HACH_ETC_SHADOW_FILE}
                 else
                     return 121 # @retval 121 - destination directory not found
                 fi
@@ -86,15 +89,15 @@ pusf__copy_shadow_file()
 #! create symbolic link as /etc/shadow -> /usr/share/hach/etc/shadow
 pusf__symlink_etc_shadow_to_user_share_hach_etc_shadow()
 {
-    if [ -f "${RFS_ETC_SHADOW_FILE}" ]
+    if [ -f "${RFS_HACH_ETC_SHADOW_FILE}" ]
     then
         if [ -n ${MV} -a -n ${LN} ]
         then
-            ${MV} "${RFS_ETC_SHADOW_FILE}" "${HACH_ETC_SHADOW_FILE}"
+            ${MV} "${RFS_ETC_SHADOW_FILE}" "${RFS_HACH_ETC_SHADOW_FILE}"
             if [ 0 -eq $? ]
             then
-                pushd "${ORIGINAL_ETC_DIR}"
-                ${LN} -sf "${HACH_ETC_SHADOW_FILE}" "${SHADOW}"
+                pushd "${RFS_ETC_DIR}"
+                ${LN} -sf ../"${HACH_ETC_DIR}/${SHADOW}" "${SHADOW}"
                 popd
             else
                 return 123 # moving the file failed
@@ -106,6 +109,7 @@ pusf__symlink_etc_shadow_to_user_share_hach_etc_shadow()
         return 127 # the RFS_SHADOW_FILE does not exist
     fi
 
+    return 0
 }
 
 
@@ -142,7 +146,11 @@ pusf__parse_file_remove_lines()
             ${GREP} -v "^${Needle}" "${File}" >> "${InterimFile}"
         fi
 
-        $(${ECHO} $AppendLine >> "${InterimFile}")
+        if [ -n "${AppendLine}" ]
+        then
+            $(${ECHO} $AppendLine >> "${InterimFile}")
+        fi
+
         ${MV} -f "${InterimFile}" "${File}"
 
     else
@@ -157,9 +165,9 @@ pusf__update_shadow_and_passwd_files()
 {
     if [ -n ${CAT} -a -n ${CUT} -a -n ${ECHO} -a -n ${GREP} ]
     then
-        if [ -n ${BR2_ROOTFS_OVERLAY} -a \
-            -f ${HACH_SHADOW_FILE}  -a \
-            -f ${RFS_PASSWD_FILE} ]
+        if [ -n "${BR2_ROOTFS_OVERLAY}" -a \
+            -f "${RFS_HACH_ETC_SHADOW_FILE}"  -a \
+            -f "${RFS_ETC_PASSWD_FILE}" ]
         then
             local RfsSourceDir=${BR2_ROOTFS_OVERLAY}/../source
             local RfsScriptsDir=${RfsSourceDir}/scripts
@@ -171,11 +179,11 @@ pusf__update_shadow_and_passwd_files()
 
                 local TargetString=$(echo "${UPDATE_PASSWD_STRING}" | "${CUT}" -d':' -f1)
 
-                pusf__parse_file_remove_lines "${HACH_SHADOW_FILE}" "${TargetString}" "${UPDATE_SHADOW_STRING}"
+                pusf__parse_file_remove_lines "${RFS_HACH_ETC_SHADOW_FILE}" "${TargetString}" "${UPDATE_SHADOW_STRING}"
 
                 if [ 0 -eq $? ]
                 then
-                    pusf__parse_file_remove_lines "${RFS_PASSWD_FILE}" "${TargetString}" "${UPDATE_PASSWD_STRING}"
+                    pusf__parse_file_remove_lines "${RFS_ETC_PASSWD_FILE}" "${TargetString}" "${UPDATE_PASSWD_STRING}"
                     return $?
                 fi
             fi
@@ -189,18 +197,29 @@ pusf__update_shadow_and_passwd_files()
 }
 
 
+
+
+
 if [ -n ${BR2_ROOTFS_OVERLAY} ]
 then
-    pusf__copy_shadow_file
-
+    pusf__update_shadow_and_passwd_files
     if [ 0 -eq $? ]
     then
-        pusf__update_shadow_and_passwd_files
-        exit $?
+        pusf__symlink_etc_shadow_to_user_share_hach_etc_shadow
     fi
 
-    exit $?
+#    pusf__copy_shadow_file
+#
+#    if [ 0 -eq $? ]
+#    then
+#        pusf__update_shadow_and_passwd_files
+#        exit $?
+#    fi
+#
+#    exit $?
 else
     exit 21 #! @retval 21 - BR2_ROOTFS_OVERLAY is not populated
 fi
+
+exit $?
 
