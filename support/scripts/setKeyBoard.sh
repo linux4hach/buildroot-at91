38a4e241d6699404c47c36e1d34e2c9536d309c5
cfair <cfair@hach.com>
2016-09-20 13:24:58 -0600

    changed keyboard back to event0 from event1

--- support/scripts/setKeyBoard.sh
+++ support/scripts/setKeyBoard.sh
@@ -14,6 +14,6 @@ if [ "$RESULTS_OF_SEARCH" == "$NONE_FOUND" ]
 then 
    echo -e "\n" >> $PROFILE
    echo "#I added this line to enable the temporary keyboard" >> $PROFILE
-   echo "export QWS_KEYBOARD=\"LinuxInput:/dev/input/event1\"" >> $PROFILE
+   echo "export QWS_KEYBOARD=\"LinuxInput:/dev/input/event0\"" >> $PROFILE
 fi
 
