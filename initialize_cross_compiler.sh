eebcb0d5c2ebd64ad79a3355104e6ccb41d8d868
cfair <cfair@hach.com>
2017-04-17 10:28:20 -0600

    fixed a logic error in the checker for whether this is a correct repostory to build everything

--- initialize_cross_compiler.sh
+++ initialize_cross_compiler.sh
@@ -3,10 +3,13 @@ SEARCH_PATH=$PWD
 REQUIRED_PART="buildroot-at91-crosscompile-root"
 if [[ ${SEARCH_PATH} != *"$REQUIRED_PART"* ]]; then
 
-   make hach-at91sam9g35_init_defconfig
-   make clean all;
- else
   echo "You are not in the required directory for this script to run, please CD to ../$REQUIRED_PART before executing this command!"
+else
+
+  make hach-at91sam9g35_init_defconfig
+  make clean all;
+
+
 fi
 
 
