fbd0fdc0d171c76e005e46e340fb603d189ea5c7
Christopher Fair <cfair@hach.com>
2017-04-20 13:55:15 -0600

    completed install-buildsystem.sh script

--- install-buildsystem.sh
+++ install-buildsystem.sh
@@ -3,20 +3,38 @@
 BASE_DIR=/opt/HachDev
 INSTALLATION_DIR=$BASE_DIR/BuildSystems/cm130 
 TEST_FOR_USER=$(whoami)
+GET_OWNER=$(ls -ld $INSTALATION_DIR | cut -d' ' -f3) 
+
+copy_files()
+{
+   cp -Rpfv * $INSTALLATION_DIR
+}
+
+set_ownership()
+{
+   sudo chown -R $TEST_FOR_USER:$TEST_FOR_USER $BASE_DIR
+}
+
 
 if [[ $TEST_FOR_USER == "root" ]]; then
    echo "Please do not run this script with sudo or as a root user!"
    exit
 fi
 
+
+
 if [ -d $INSTALATION_DIR ]
 then
    
-   cp -Rpfv * $INSTALLATION_DIR
+   if [[ $GET_OWNER != $TEST_FOR_USER ]]; then
+      set_ownership
+
+   fi
+   copy_files
 
 else
-   echo "The installation directory does not already exist, please type sudo mkdir -p $INSTALATION_DIR  then type chown -R $USER:$USER $BASE_DIR"
-
+   sudo mkdir -p $INSTALLATION_DIR
+   set_ownership
 fi
 
 
