c7ea3ca10d0f55a42608b432ab75a50a2de30de9
cfair <cfair@hach.com>
2017-11-30 16:34:00 -0700

    made convert to sdk more generic

--- convert_to_sdk.sh
+++ convert_to_sdk.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 echo "Converting buildroot into a bare sdk for downloading"
-cd /opt/hachdev/sdk/r1702/buildroot
+current_dir=$(pwd)
+cd $current_dir 
 rm -rf fs arch board boot configs dl docs linux package support system toolchain utils sdcard
 rm *
-cd /opt/hachdev/sdk/r1702/buildroot/output
+cd $current_dir/output
 rm -rf build images staging target
 echo "Conversion to sdk is complete"
 
