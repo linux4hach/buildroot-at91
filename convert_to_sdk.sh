3714beffd802ff11cd24960a044b5197bab3c0a8
cfair <cfair@hach.com>
2017-11-30 14:31:25 -0700

    removes all superfelous information

--- convert_to_sdk.sh
+++ convert_to_sdk.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 echo "Converting buildroot into a bare sdk for downloading"
-cd /opt/hachdev/sdk/r1702/buildroot
+name_of_dir=$(pwd)
+cd $name_of_dir 
 rm -rf fs arch board boot configs dl docs linux package support system toolchain utils
 rm *
-cd /opt/hachdev/sdk/r1702/buildroot/output
+cd $name_of_dir/output
 rm -rf build images staging target
 echo "Conversion to sdk is complete"
 
