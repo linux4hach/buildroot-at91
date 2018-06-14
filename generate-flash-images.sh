9f545d2d15254327235ed8f228dfa1eb5a45d109
cfair <cfair@hach.com>
2018-06-14 13:15:55 -0600

    updated overlay

--- generate-flash-images.sh
+++ generate-flash-images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-unset PERL_MM_OPT
-MAKE_DIR=$(./compileCustomMake.sh)
-export PATH=${MAKE_DIR}:${PATH}
+#unset PERL_MM_OPT
+#MAKE_DIR=$(./compileCustomMake.sh)
+#export PATH=${MAKE_DIR}:${PATH}
 make hach-at91sam9g35_nor_defconfig
-${MAKE_DIR}/make "$@"
+make "$@"
 
