978acba40df3a9025d4faf617a83931ba5b28ede
developer <developer@hach.com>
2018-06-14 13:17:28 -0600

    removing custom make file generation

--- generate-flash-images.sh
+++ generate-flash-images.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-#unset PERL_MM_OPT
-#MAKE_DIR=$(./compileCustomMake.sh)
-#export PATH=${MAKE_DIR}:${PATH}
 make hach-at91sam9g35_nor_defconfig
 make "$@"
 
--- generate-sd-images.sh
+++ generate-sd-images.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-unset PERL_MM_OPT
-MAKE_DIR=$(./compileCustomMake.sh)
-export PATH=${MAKE_DIR}:$PATH
 make  O=./sdcard hach-at91sam9g35_sdcard_defconfig "$@"
 make  O=./sdcard "$@"
